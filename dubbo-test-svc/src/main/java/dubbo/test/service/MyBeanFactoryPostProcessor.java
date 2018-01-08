8ded0fd9cdd45ebfba8decbf8a9268ad8fab7624
Richard Liu <liuyongcheng@huli.com>
2018-01-08 17:24:40 +0800

    add BeanFactoryPostProcessor

---
+++ dubbo-test-svc/src/main/java/dubbo/test/service/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,26 @@
+package dubbo.test.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Richard on 1/8/18.
+ */
+@Component
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyBeanFactoryPostProcessor.class);
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+        beanFactory.registerSingleton("myPostBean", dubbo.test.service.impl.NewServiceImpl.class);
+        LOGGER.info("class is {}", beanFactory.getClass().getName());
+
+
+    }
+}
